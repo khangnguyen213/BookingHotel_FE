1cae707d27e9f5e2f885e406c140521a31ae6b4c
khangnguyen213 <khangnguyenm01@gmail.com>
2023-12-22 10:03:46 +0700

    Update API for deploy

--- src/global.js
+++ src/global.js
@@ -1,4 +1,4 @@
 export default class Global {
-  static BASE_BACKEND_API = 'http://localhost:5000';
-  // static BASE_BACKEND_API = 'https://charitee-be.vercel.app';
+  // static BASE_BACKEND_API = 'http://localhost:5000';
+  static BASE_BACKEND_API = 'https://booking-hotel-be.vercel.app/';
 }
